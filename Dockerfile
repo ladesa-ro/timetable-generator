ade680aa2b48ede1d79f8c86289ab52a88fd9718
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2025-10-12 18:53:41 -0400

    build(ci/cd): add CI/CD workflow for Docker and Kubernetes deployment
    - Introduced a GitHub Actions workflow for container image build and Kubernetes deployment.
    - Configured Docker image publishing to GitHub Container Registry.
    - Added Kubernetes YAML files for deployment and service in the development namespace.
    - Included a placeholder deployment script for Kubernetes.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM mcr.microsoft.com/dotnet/sdk:9.0 AS tool-csharpier
-
 RUN dotnet tool install csharpier --tool-path /bin 
-
 ENTRYPOINT [ "csharpier" ]
+
+
+FROM docker.io/alpine AS timetable-generator-runtime
+
+ENTRYPOINT ["sleep", "infinity"]
---
+++ k8s/development/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -xeo pipefail;
+
+#kubectl apply -f ./deployments/timetable-generator.yaml;
+#kubectl apply -f ./services/timetable-generator.yaml;
+#
+#kubectl rollout restart \
+#  deployment.apps/ladesa-ro-timetable-generator \
+#  --namespace ladesa-ro-development \
+#;
