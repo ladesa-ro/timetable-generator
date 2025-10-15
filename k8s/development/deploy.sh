5f8521bd34df7c290299d55e32b24c30ff7cba52
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2025-10-14 22:42:35 -0400

    fix(deployment): improve rollout process for timetable generator
    - Added rollout status check to ensure successful deployment.
    - Removed redundant semicolons for better script consistency.
    - Enhanced readability with comments detailing each step.

--- k8s/development/deploy.sh
+++ k8s/development/deploy.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-set -xeo pipefail;
+set -xeo pipefail
 
-kubectl apply -f ./secrets/timetable-generator.yaml;
-kubectl apply -f ./deployments/timetable-generator.yaml;
+# Aplica secrets e deployment
+kubectl apply -f ./secrets/timetable-generator.yaml
+kubectl apply -f ./deployments/timetable-generator.yaml
 
+# Reinicia o deployment
 kubectl rollout restart \
   deployment.apps/ladesa-ro-timetable-generator \
-  --namespace ladesa-ro-development \
-;
+  --namespace ladesa-ro-development
+
+# Aguarda o rollout finalizar
+kubectl rollout status \
+  deployment/ladesa-ro-timetable-generator \
+  --namespace ladesa-ro-development
