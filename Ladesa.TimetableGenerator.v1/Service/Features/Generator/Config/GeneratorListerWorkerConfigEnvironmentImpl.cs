9acc4636b25327d9a9f7f786563c9bb87cbb8cc9
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2025-10-20 07:42:05 -0400

    chore: fix typo in environment variable name for consistency

--- Ladesa.TimetableGenerator.v1/Service/Features/Generator/Config/GeneratorListerWorkerConfigEnvironmentImpl.cs
+++ Ladesa.TimetableGenerator.v1/Service/Features/Generator/Config/GeneratorListerWorkerConfigEnvironmentImpl.cs
@@ -2,7 +2,7 @@ namespace Ladesa.TimetableGenerator.v1.Service.Features.Generator.Config;
 
 public class GeneratorListerWorkerConfigEnvironmentImpl (IConfiguration config): IGeneratorListenWorkerConfig
 {
-    private const string QueueListen = "TIMETABLE_SERVICEC_BROKER_QUEUE_LISTEN";
+    private const string QueueListen = "TIMETABLE_SERVICE_BROKER_QUEUE_LISTEN";
     private const string QueueReply = "TIMETABLE_SERVICE_BROKER_QUEUE_REPLY";
 
     public IGeneratorListenWorkerConfig.GeneratorListenWorkerConfig GetConfig()
