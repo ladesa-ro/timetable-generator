0d70af1064ebbcd85f4352be5b2d3ae63e2671c6
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2026-02-06 16:58:31 -0400

    chore: ci/cd

--- k8s/development/deploy.sh
+++ k8s/development/deploy.sh
@@ -2,16 +2,17 @@
 
 set -xeo pipefail
 
+kubectl apply -f ./secrets/timetable-generator.yaml
 kubectl apply -f ./deployments/timetable-generator.yaml
 kubectl apply -f ./services/timetable-generator.yaml
 
 # Reinicia o deployment
 kubectl rollout restart \
-  deployment.apps/timetable-generator \
-  --namespace ladesa
+  deployment.apps/ladesa-ro-timetable-generator \
+  --namespace ladesa-ro-development
 
 # Aguarda o rollout finalizar
 kubectl rollout status \
-  deployment/timetable-generator \
-  --namespace ladesa \
+  deployment/ladesa-ro-timetable-generator \
+  --namespace ladesa-ro-development \
   --timeout=300s
